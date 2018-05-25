b70faa5731c5dee0b6015a54eeec031c68a8e34a
daddabarba <davidebarbieri97@gmail.com>
2018-05-25 14:10:13 +0200

    working grasp per class

--- scripts/graspPars.py
+++ scripts/graspPars.py
@@ -1,30 +1,41 @@
-TARGET_SIZE = [0.05, 0.16, 0.06]  
+TARGET_SIZE = [0.07, 0.16, 0.07] #[0.05, 0.16, 0.06]  
 
 TARGET_POSE = {
-	"x": 0.2007, #0.24
-	"y": -0.332, #-0.32
-	"z": 0.04,
+	"x": 0.201, #0.2007, #0.24
+	"y": -0.34, #-0.332, #-0.32
+	"z": -0.015,
 	"w": 1.0
 }
 
 
+GRASPING_HEIGHT = 0.02
+
 ORIENTATIONS = {}
 TARGET_SIZES = {}
+TARGET_POSES = {}
 
 for i in range(1,11):
 
 	ORIENTATIONS[i] = {
-		"yaw": ((i-1)*20)%180,
+		"yaw": -((i-1)*20)%180,
 		"pitch": 90,
 		"roll": 0
 	}
 
 	TARGET_SIZES[i] = TARGET_SIZE
+	
+TARGET_SIZES[10] = [0.06, 0.06, 0.15]
 
-TARGET_SIZE[10] = [0.06, 0.06, 0.1]
 
+for i in range(1,11):
+	TARGET_POSES[i] = {
+		"x": TARGET_POSE["x"],
+		"y": TARGET_POSE["y"],
+		"z": TARGET_POSE["z"] + TARGET_SIZES[i][2]/2.0,
+		"w": TARGET_POSE["w"]
+	}
 
-ORIENTATIONS_GRASP = copy(ORIENTATIONS)
+ORIENTATIONS_GRASP = ORIENTATIONS.copy()
 
 ORIENTATIONS_GRASP[10] = { 
 	"yaw": 90,
--- scripts/grasp_tutorial.py
+++ scripts/grasp_tutorial.py
@@ -14,10 +14,10 @@ import numpy as np
 import graspPars as pars
 
 import sys
+import copy
 
 class MoveItTutorial(object):
-    def __init__(self, nClass):
-    
+    def __init__(self):
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node("grasp_tutorial")
         
@@ -25,43 +25,51 @@ class MoveItTutorial(object):
         self.scene_pub = rospy.Publisher("planning_scene", PlanningScene, queue_size=10)
         self.gripper_pose_pub = rospy.Publisher("target_pose", Pose, queue_size=10)
 
-        arm = MoveGroupCommander("arm")
+        self.arm = MoveGroupCommander("arm")
         gripper = MoveGroupCommander("gripper")
 
-        end_effector_link = arm.get_end_effector_link()
+        end_effector_link = self.arm.get_end_effector_link()
 
-        arm.allow_replanning(True)
-        arm.set_pose_reference_frame("world")
+        self.arm.allow_replanning(True)
+        self.arm.set_pose_reference_frame("world")
 
-        arm.set_planning_time(5)
+        self.arm.set_planning_time(5)
     
         rospy.sleep(2)
         
-        box1_id = "box1"
-        target_id = "target"
+        self.box1_id = "box1"
+        self.target_id = "target"
 
-        self.scene.remove_world_object(box1_id)
-        self.scene.remove_world_object(target_id)
+        self.scene.remove_world_object(self.box1_id)
+        self.scene.remove_world_object(self.target_id)
 
-        self.scene.remove_attached_object("m1n6s200_end_effector", target_id)
+        self.scene.remove_attached_object("m1n6s200_end_effector", self.target_id)
 
         rospy.sleep(1)
 
 
-        target_size = copy(pars.TARGET_SIZES[nClass])
+    def act_grasp(self, nClass):
+        self.nClass = nClass
+
+        target_size = copy.copy(pars.TARGET_SIZES[nClass])
         target_pose = PoseStamped()
         target_pose.header.frame_id = "/world"
-        target_pose.pose.position.x = pars.TARGET_POSE["x"]
-        target_pose.pose.position.y = pars.TARGET_POSE["y"]
-        target_pose.pose.position.z = pars.TARGET_POSE["z"]
-        target_pose.pose.orientation.w = pars.TARGET_POSE["w"]
+        target_pose.pose.position.x = pars.TARGET_POSES[nClass]["x"]
+        target_pose.pose.position.y = pars.TARGET_POSES[nClass]["y"]
+        target_pose.pose.position.z = pars.TARGET_POSES[nClass]["z"]
+        target_pose.pose.orientation.w = pars.TARGET_POSES[nClass]["w"]
 
-       	target_pose = self.rotate(pars.ORIENTATIONS[nClass]["yaw"], pars.ORIENTATIONS[nClass]["pitch"], pars.ORIENTATIONS[nClass]["roll"])
+       	target_pose = self.rotate(target_pose, pars.ORIENTATIONS[nClass]["yaw"], pars.ORIENTATIONS[nClass]["pitch"], pars.ORIENTATIONS[nClass]["roll"])
 
-        self.scene.add_box(target_id, target_pose, target_size)
+        self.scene.add_box(self.target_id, target_pose, target_size)
         rospy.sleep(0.5)
 
-        grasp_pose = copy(target_pose)
+        grasp_pose = copy.copy(target_pose)
+        grasp_pose.pose.position.z = pars.GRASPING_HEIGHT
+
+        print "x: " + str(grasp_pose.pose.position.x) + " y: " + str(grasp_pose.pose.position.y) + " z: " + str(grasp_pose.pose.position.z)
+        print "x: " + str(target_pose.pose.position.x) + " y: " + str(target_pose.pose.position.y) + " z: " + str(target_pose.pose.position.z)
+
 
         '''
         grasp_pose = PoseStamped()
@@ -72,9 +80,9 @@ class MoveItTutorial(object):
         grasp_pose.pose.orientation.w = 1.0 
         '''
 
-        grasps = self.make_grasps(grasp_pose, [target_id])
+        grasps = self.make_grasps(grasp_pose, [self.target_id])
              
-        result = arm.pick(target_id, grasps)
+        result = self.arm.pick(self.target_id, grasps)
         if result == MoveItErrorCodes.SUCCESS:
             print 'Success'
         else:
@@ -83,13 +91,13 @@ class MoveItTutorial(object):
         moveit_commander.roscpp_shutdown()
         moveit_commander.os._exit(0)
 
-    def rotate(pose, yaw, pitch, roll):
+    def rotate(self, pose, yaw, pitch, roll):
     	q = quaternion_from_euler(math.radians(pitch) + math.pi / 2, math.radians(roll), math.radians(yaw))
 
         pose.pose.orientation.x = q[0]
-        pose.pose.orientation.x = q[1]
-        pose.pose.orientation.x = q[2]
-        pose.pose.orientation.x = q[3]
+        pose.pose.orientation.y = q[1]
+        pose.pose.orientation.z = q[2]
+        pose.pose.orientation.w = q[3]
 
         return pose
 
@@ -180,9 +188,10 @@ class MoveItTutorial(object):
 
 
 if __name__ == "__main__":
-	nClass = 1
+    nClass = 1
 
-	if len(sys.argv)>1:
-		nClass = sys.argv[1]
+    if len(sys.argv)>1:
+        nClass = int(sys.argv[1])
 
-    moveit_tutorial = MoveItTutorial(nClass)
+    moveit_tutorial = MoveItTutorial()
+    moveit_tutorial.act_grasp(nClass)
